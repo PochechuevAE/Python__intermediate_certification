4b118da293f97f2de60e6a2da35975cc5f1b8faa
PochechuevAE <trakturus@list.ru>
2024-01-20 00:52:59 +0300

    add view_all_notes func in Notes_functions

--- Notes_functions.py
+++ Notes_functions.py
@@ -14,5 +14,16 @@ def save_notes(notes):
         json.dump(notes, file, indent=2)
         
 def add_notes(notes, data):
-    notes.append({"Идентификатор": data[0], "Заголовок": data[1], "Тело заметки": data[2], "дату/время создания/изменения": data[3]})
+    notes.append({"Идентификатор": data[0], "Заголовок": data[1], "Тело заметки": data[2], "Дата/время создания/изменения": data[3]})
     save_notes(notes)
+    
+def view_all_notes(notes):
+    if not notes:
+        msgbox("Справочник пуст", 'Телефонный справочник')
+        return
+
+    msg = ""
+    for notes in notes:
+        msg += f"{notes['Идентификатор']} {notes['Заголовок']} {notes['Тело заметки']}: {notes['Дата/время создания/изменения']}\n"
+    
+    msgbox(msg, 'Программа для заметок')
